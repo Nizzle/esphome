3e14a127746c56e800b6b665fdd79322f84de798
The_Niz <the_niz@nurdspace.nl>
2022-12-06 19:41:10 +0100

    Initial support for https://www.badge.team/docs/badges/sha2017/hardware/DEPG0290B01V3.0.pdf (full updates only for now)

--- esphome/components/waveshare_epaper/display.py
+++ esphome/components/waveshare_epaper/display.py
@@ -36,6 +36,9 @@ WaveshareEPaper2P9InBV3 = waveshare_epaper_ns.class_(
     "WaveshareEPaper2P9InBV3", WaveshareEPaper
 )
 GDEY029T94 = waveshare_epaper_ns.class_("GDEY029T94", WaveshareEPaper)
+WaveshareEPaper2P9InDKE = waveshare_epaper_ns.class_(
+    "WaveshareEPaper2P9InDKE", WaveshareEPaper
+)
 WaveshareEPaper4P2In = waveshare_epaper_ns.class_(
     "WaveshareEPaper4P2In", WaveshareEPaper
 )
@@ -92,6 +95,7 @@ MODELS = {
     "2.70inv2": ("b", WaveshareEPaper2P7InV2),
     "2.90in-b": ("b", WaveshareEPaper2P9InB),
     "2.90in-bv3": ("b", WaveshareEPaper2P9InBV3),
+    "2.90in-dke": ("c", WaveshareEPaper2P9InDKE),
     "4.20in": ("b", WaveshareEPaper4P2In),
     "4.20in-bv2": ("b", WaveshareEPaper4P2InBV2),
     "5.83in": ("b", WaveshareEPaper5P8In),
